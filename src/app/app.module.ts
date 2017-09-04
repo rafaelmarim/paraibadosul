70cf25a80af634a1605510311823279fb0b5f8a1
Rafael Marim de Souza <rafaelmarimdesouza@MacBook-Air-de-Rafael.local>
2017-09-04 14:03:35 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPageModule } from "../pages/intro/intro.module";
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IntroPageModule 
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
